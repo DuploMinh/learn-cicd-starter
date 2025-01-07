4e3b33116d0859cf2b2fe3ec73a8bd9c9b89e7ed
Minh Vu <minh.duplo@gmail.com>
2025-01-07 20:44:41 +0700

    added tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -21,40 +21,50 @@ func TestGetAPIKey(t *testing.T) {
 			want:        "some_valid_key",
 			expectError: nil,
 		},
-		// {
-		// 	name: "Missing Authorization Header",
-		// 	headers:     http.Header{},
-		// 	want:        "",
-		// 	expectError: ErrNoAuthHeaderIncluded,
-		// },
-		// {
-		// 	name: "Malformed Authorization Header",
-		// 	headers: http.Header{
-		// 		"Authorization": []string{"Bearer some_token"},
-		// 	},
-		// 	want:        "",
-		// 	expectError: errors.New("malformed authorization header"),
-		// },
-		// {
-		// 	name: "Empty Authorization Header",
-		// 	headers: http.Header{
-		// 		"Authorization": []string{""},
-		// 	},
-		// 	want:        "",
-		// 	expectError: ErrNoAuthHeaderIncluded,
-		// },
+		{
+			name: "Missing Authorization Header",
+			headers:     http.Header{},
+			want:        "",
+			expectError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization Header (Invalid Scheme)",
+			headers: http.Header{
+				"Authorization": []string{"Bearer some_token"},
+			},
+			want:        "",
+			expectError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Malformed Authorization Header (No Key Provided)",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey"},
+			},
+			want:        "",
+			expectError: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Empty Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{""},
+			},
+			want:        "",
+			expectError: ErrNoAuthHeaderIncluded,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GetAPIKey(tt.headers)
 
-			// Check the expected error
-			if !errors.Is(err, tt.expectError) {
+			// Check for expected error
+			if (err != nil && tt.expectError == nil) || (err == nil && tt.expectError != nil) {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.expectError)
+			} else if err != nil && tt.expectError != nil && err.Error() != tt.expectError.Error() {
 				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.expectError)
 			}
 
-			// Check the expected result
+			// Check for expected result
 			if got != tt.want {
 				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
 			}
